169d833e1864989ae58ae24dc1f65b9ef63f6dfe
promise310 <seoyoung22dev@gmail.com>
2023-05-23 23:59:45 -0700

    PORTFOLIO

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,11 +48,20 @@ tabs.forEach(tab => {
     })
 })
 
-/*==================== SERVICES MODAL ====================*/
-
-
 /*==================== PORTFOLIO SWIPER  ====================*/
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
 
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
