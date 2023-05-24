52a3b6e28d22f1ecf5f7a52afe9ba8981905c919
promise310 <seoyoung22dev@gmail.com>
2023-05-24 16:03:43 -0700

    Email

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,8 +63,51 @@ let swiper = new Swiper(".portfolio__container", {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
+/*==================== EMAIL JS ====================*/
+const contactForm = document.getElementById('contact-form'),
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactProject = document.getElementById('contact-project'),
+contactMessage = document.getElementById('contact-message'),
+contactAlarm = document.getElementById('contact-alarm')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //Check if the field has a value
+    if(contactName?.value === '' || contactEmail?.value === '' || contactProject?.value === '' || contactMessage?.value === ''){
+        //Add and remove color
+        contactAlarm.classList.remove('color-blue')
+        contactAlarm.classList.add('color-red')
+
+        //Show message
+        contactAlarm.textContent = 'Write all the input fields 📥'
+    }else{
+        //serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_sy310','template_email','#contact-form','wXqOm3sFtvI7cK8h2')
+        .then(() =>{
+            //Show message and add color
+            contactAlarm.classList.add('color-blue')
+            contactAlarm.textContent = 'Message sent ✅'
+            console.log('SUCCESS!');
+
+            //Remove message after five second
+            setTimeout(()=>{
+                contactAlarm.textContent = ''
+            }, 5000)
+        },(err) =>{
+            alert('Something has failed...', err)
+        })
+
+        //To clear the input field
+        contactName.value = ''
+        contactEmail.value = ''
+        contactProject.value = ''
+        contactMessage.value = ''
+        contactAlarm.value = ''
+    }
+}
+contactForm.addEventListener('submit', sendEmail)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
