4a77ecc9496afb78c7dc0af81e80facebc5d5f58
promise310 <seoyoung22dev@gmail.com>
2023-05-23 19:53:41 -0700

    Qualification

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,11 +28,25 @@ function linkAction(){
 }
 navLink.forEach(n=> n.addEventListener('click', linkAction))
 
-/*==================== ACCORDION SKILLS ====================*/
-
-
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
